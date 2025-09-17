4c3341ec188e414688fea5c4e6a610edd03a6fa7
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-09-17 20:01:37 +0530

    user up

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,31 @@
 //  IMP:  # User Defination in database =  Interface IUser extends Document.
 import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
 
 export interface IUser extends Document {
-  username: string;
+  name: string;
+  email: string;
   password: string;
+  comparePassword(password: string): Promise<boolean>;
 }
 
 const userSchema = new Schema<IUser>({
-  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare password method
+userSchema.methods.comparePassword = async function (password: string) {
+  return bcrypt.compare(password, this.password);
+};
 
 export default mongoose.model<IUser>("User", userSchema);
