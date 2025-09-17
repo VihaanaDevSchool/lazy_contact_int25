ac4d54ebb1da49f97adada8545a4ad39622c5c33
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-09-17 17:00:42 +0530

    index.ts

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors"; // diddleware for {ports, requests, frontend}
+import { connectDB } from "./config/db"; // database
+import authRoutes from "./routes/authRoutes";
+import contactRoutes from "./routes/contactRoutes";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend API is working 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("🚀 Server running at http://localhost:5000");
+});
