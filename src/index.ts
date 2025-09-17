c37da6521d49e8d4b848ed750a071ef0423364f2
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-09-18 03:25:56 +0530

    intexts

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,38 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db";
 import contactRoutes from "./routes/contactRoutes";
+import authRoutes from "./routes/authRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const HOST = "0.0.0.0";
-
-app.listen(PORT, HOST, () => {
-  console.log(`🚀 Server running at http://${HOST}:${PORT}`);
-});
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
 app.use("/api/contacts", contactRoutes);
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"], // frontend dev server
+    credentials: true,
+  }),
+);
 
 // Root test
 app.get("/", (_req, res) => {
   res.send("🚀 API is running inside Docker!");
 });
 
-// Start server
-app.listen(PORT, HOST, async () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-  await connectDB();
+// Connect DB and start server
+connectDB().then(() => {
+  app.listen(PORT, HOST, () => {
+    console.log(`🚀 Server running at http://${HOST}:${PORT}`);
+  });
 });
-
 // Y:
 // ----------------
 // IP find
